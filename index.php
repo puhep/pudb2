3aef893f63b24f185e972f1ee54cd472ee379273
Gregory W Neeser <gneeser@purdue.edu>
2017-02-13 14:51:22 -0500

    First Commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<title>Purdue CMS FPix Phase II R&D Database</title>
+<body>
+<img src="../phase_2/pics/pu_logo.jpg" width="200" height="100">
+<img src="../phase_2/pics/CMS_logo_col.gif" width="100" height="100">
+<br>
+<h1>Purdue CMS FPix Phase II R&D Database</h1>
+
+<h2>Part List</h2>
+
+<input type=button onClick="location.href='part_list.php'" value='Part List'>
+
+<h2>Reference</h2>
+To Do: naming conventions, etc.
+
+<h2>Links</h2>
+
+<input type="button" value="Project Logbook"
+onclick="window.open('https://docs.google.com/document/d/1zDu6hiUR7r6qumQPcKdV3OXh7vLpGjodTjLopjbufKQ/edit?usp=sharing')">
+<br><br>
+<input type="button" value="Project Google Drive"
+onclick="window.open('https://drive.google.com/drive/folders/0B04OIAGnMDYxbXBkTWJmMm5hN0E?usp=sharing')">
+
+</body>
+</html>
---
+++ part_list.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>Part List</title>
+
+<body>
+<h1>Part List</h1>
+<p>[construction noises]</p>
+
+
+
+
+
+<input type=button onClick="location.href='index.php'" value='Index'>
+
+
+</body>
+</html>
