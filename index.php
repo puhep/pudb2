f129c0874dd9e07c608b605de7d0de8a8351e5f5
Chase Nelson <chase.a.nelson@gmail.com>
2017-05-18 13:51:35 -0400

    Added header

--- index.php
+++ index.php
@@ -1,43 +1,34 @@
+<!DOCTYPE html>
 <html>
-<head>
-<title>Purdue CMS FPix Phase II R&D Database</title>
-<body>
-<img src="../phase_2/pics/pu_logo.jpg" width="200" height="100">
-     <img src="../phase_2/pics/CMS_logo_col.gif" width="100" height="100">
-     <br>
-     <h1>Purdue CMS FPix Phase II R&D Database</h1>
-
-     <h2>Part Information</h2>
-
-     <input type=button onClick="location.href='part_list.php'" value='Part List'>
-
-     <h2>Part Submission</h2>
-
-     <input type=button onClick="location.href='newsheet.php'" value='Add Sheet'>
-
-     <h2>Testing Information</h2>
-
-     <input type=button onClick="location.href='test_list.php'" value='Test List'>
-
-     <input type=button onClick="location.href='newtest.php'" value='New Test'>
-
-
-     <!--
-     <h2>Reference</h2>
-     To Do: naming conventions, etc.
-     -->
-
-  <h2>Links</h2>
-
-   <input type="button" value="Project Logbook"
-   onclick="window.open('https://docs.google.com/document/d/1zDu6hiUR7r6qumQPcKdV3OXh7vLpGjodTjLopjbufKQ/edit?usp=sharing')">
-   <br><br>
-   <input type="button" value="Project Google Drive"
-   onclick="window.open('https://drive.google.com/drive/folders/0B04OIAGnMDYxbXBkTWJmMm5hN0E?usp=sharing')">
-
-   <div>
-     <p style="font-size:.7em; font-style:italic;">This site uses HTML5. Please use Google Chrome, Mozilla Firefox, Safari, or IE9 (or greater).</p>
-   </div>
+  <head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Purdue CMS FPix Phase II R&D Database</title>
+  </head>
+  <body>
+    <a href="index.php">
+      <div class="header">
+        <img src="../phase_2/pics/pu_logo.jpg" width="200" height="100">
+        <img src="../phase_2/pics/CMS_logo_col.gif" width="100" height="100">
+      </div>
+    </a>  
+    <h1>Purdue CMS FPix Phase II R&D Database</h1>
+    <h2>Part Information</h2>
+    <input type=button onClick="location.href='part_list.php'" value='Part List'>
+    <h2>Part Submission</h2>
+    <input type=button onClick="location.href='newsheet.php'" value='Add Sheet'>
+    <h2>Testing Information</h2>
+    <input type=button onClick="location.href='test_list.php'" value='Test List'>
+    <input type=button onClick="location.href='newtest.php'" value='New Test'>
+     <!-- <h2>Reference</h2> To Do: naming conventions, etc. -->
+    <h2>Links</h2>
+    <input type="button" value="Project Logbook"
+    onclick="window.open('https://docs.google.com/document/d/1zDu6hiUR7r6qumQPcKdV3OXh7vLpGjodTjLopjbufKQ/edit?usp=sharing')">
+    <br><br>
+    <input type="button" value="Project Google Drive"
+    onclick="window.open('https://drive.google.com/drive/folders/0B04OIAGnMDYxbXBkTWJmMm5hN0E?usp=sharing')">
+    <div class="foot">
+      <p>This site uses HTML5. Please use Google Chrome, Mozilla Firefox, Safari, or IE9 (or greater).</p>
+    </div>
  </body>
 </html>
 
