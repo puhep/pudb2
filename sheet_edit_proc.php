a270523d6f95ac75c0baa4b99d17153e9a1f38de
Chase Nelson <chase.a.nelson@gmail.com>
2017-05-26 16:23:10 -0400

    reformatted document

--- sheet_edit_proc.php
+++ sheet_edit_proc.php
@@ -9,13 +9,13 @@ $id = $_POST['id'];
 $db = new Database();
 
 //print_r($_POST);
-### update miscellaneous info if the fields have been filled
-if($_POST['name'] != ""){
-    $sql = "update sheet set name=\"".$_POST['name']."\" where id=$id";
+### UPDATE miscellaneous info if the fields have been filled
+if ($_POST['name'] != "") {
+    $sql = "UPDATE sheet SET name=\"".$_POST['name']."\" WHERE id=$id";
     $db -> query($sql);
 }
-if($_POST['location'] != ""){
-    $sql = "update sheet set location = \"".$_POST['location']."\" where id=$id";
+if ($_POST['location'] != "") {
+    $sql = "UPDATE sheet SET location = \"".$_POST['location']."\" WHERE id=$id";
     $db -> query($sql);
 }
 
@@ -34,7 +34,7 @@ if ($_POST['bagUseTimes'] != "") {
   $db->query($sql);
 }
 
-if ($_POST['ovenStart'] != ""){
+if ($_POST['ovenStart'] != "") {
   $sql = "UPDATE sheet SET ovenStart=\"".$_POST['ovenStart']."\" WHERE id=$id";
   $db->query($sql);
 }
@@ -44,7 +44,7 @@ if ($_POST['ovenReach177'] != "") {
   $db->query($sql);
 }
 
-if ($_POST['ovenReach107'] != ""){
+if ($_POST['ovenReach107'] != "") {
   $sql = "UPDATE sheet SET ovenReach107=\"".$_POST['ovenReach107']."\" WHERE id=$id";
   $db->query($sql);
 }
@@ -89,78 +89,78 @@ if ($_POST['bow'] != "") {
   $db->query($sql);
 }
 
-if($_POST['ply'] != ""){
-    $sql = "update sheet set ply=".$_POST['ply']." where id=$id";
+if ($_POST['ply'] != "") {
+    $sql = "UPDATE sheet SET ply=".$_POST['ply']." WHERE id=$id";
     $db -> query($sql);
 }
-if($_POST['mass_nb'] != ""){
-    $sql = "update sheet set mass_nb=".$_POST['mass_nb']." where id=$id";
+if ($_POST['mass_nb'] != "") {
+    $sql = "UPDATE sheet SET mass_nb=".$_POST['mass_nb']." WHERE id=$id";
     $db -> query($sql);
 }
-if($_POST['mass_after'] != ""){
-    $sql = "update sheet set mass_after=".$_POST['mass_after']." where id=$id";
+if ($_POST['mass_after'] != "") {
+    $sql = "UPDATE sheet SET mass_after=".$_POST['mass_after']." WHERE id=$id";
     $db -> query($sql);
 }
-if($_POST['num_wax_coats'] != ""){
-    $sql = "update sheet set num_wax_coats=".$_POST['num_wax_coats']." where id=$id";
+if ($_POST['num_wax_coats'] != "") {
+    $sql = "UPDATE sheet SET num_wax_coats=".$_POST['num_wax_coats']." WHERE id=$id";
     $db -> query($sql);
 }
-if($_POST['curing_stackup'] != ""){
-    $sql = "update sheet set curing_stackup=\"".$_POST['curing_stackup']."\" where id=$id";
+if ($_POST['curing_stackup'] != "") {
+    $sql = "UPDATE sheet SET curing_stackup=\"".$_POST['curing_stackup']."\" WHERE id=$id";
     $db -> query($sql);
 }
-if($_POST['user_cut'] != ""){
-    $sql = "update sheet set user_cut=\"".$_POST['user_cut']."\" where id=$id";
+if ($_POST['user_cut'] != "") {
+    $sql = "UPDATE sheet SET user_cut=\"".$_POST['user_cut']."\" WHERE id=$id";
     $db -> query($sql);
 }
-if($_POST['user_bagged'] != ""){
-    $sql = "update sheet set user_bagged=\"".$_POST['user_bagged']."\" where id=$id";
+if ($_POST['user_bagged'] != "") {
+    $sql = "UPDATE sheet SET user_bagged=\"".$_POST['user_bagged']."\" WHERE id=$id";
     $db -> query($sql);
 }
-if($_POST['user_check1'] != ""){
+if ($_POST['user_check1'] != "") {
     #echo "found";
-    $sql = "update sheet set user_check1=\"".$_POST['user_check1']."\" where id=$id";
+    $sql = "UPDATE sheet SET user_check1=\"".$_POST['user_check1']."\" WHERE id=$id";
     $db -> query($sql);
 }
-if($_POST['user_check2'] != ""){
-    $sql = "update sheet set user_check2=\"".$_POST['user_check2']."\" where id=$id";
+if ($_POST['user_check2'] != "") {
+    $sql = "UPDATE sheet SET user_check2=\"".$_POST['user_check2']."\" WHERE id=$id";
     $db -> query($sql);
 }
-if($_POST['user_check3'] != ""){
-    $sql = "update sheet set user_check3=\"".$_POST['user_check3']."\" where id=$id";
+if ($_POST['user_check3'] != "") {
+    $sql = "UPDATE sheet SET user_check3=\"".$_POST['user_check3']."\" WHERE id=$id";
     $db -> query($sql);
 }
-if($_POST['user_ramp'] != ""){
-    $sql = "update sheet set user_ramp=\"".$_POST['user_ramp']."\" where id=$id";
+if ($_POST['user_ramp'] != "") {
+    $sql = "UPDATE sheet SET user_ramp=\"".$_POST['user_ramp']."\" WHERE id=$id";
     $db -> query($sql);
 }
-if($_POST['user_measure'] != ""){
-    $sql = "update sheet set user_measure=\"".$_POST['user_measure']."\" where id=$id";
+if ($_POST['user_measure'] != "") {
+    $sql = "UPDATE sheet SET user_measure=\"".$_POST['user_measure']."\" WHERE id=$id";
     $db -> query($sql);
 }
-if($_POST['user_remove'] != ""){
-    $sql = "update sheet set user_remove=\"".$_POST['user_remove']."\" where id=$id";
+if ($_POST['user_remove'] != "") {
+    $sql = "UPDATE sheet SET user_remove=\"".$_POST['user_remove']."\" WHERE id=$id";
     $db -> query($sql);
 }
-if($_POST['thickness1'] != ""){
-    $sql = "update sheet set thickness1=".$_POST['thickness1']." where id=$id";
+if ($_POST['thickness1'] != "") {
+    $sql = "UPDATE sheet SET thickness1=".$_POST['thickness1']." WHERE id=$id";
     $db -> query($sql);
 }
-if($_POST['thickness2'] != ""){
-    $sql = "update sheet set thickness2=".$_POST['thickness2']." where id=$id";
+if ($_POST['thickness2'] != "") {
+    $sql = "UPDATE sheet SET thickness2=".$_POST['thickness2']." WHERE id=$id";
     $db -> query($sql);
 }
-if($_POST['thickness3'] != ""){
-    $sql = "update sheet set thickness3=".$_POST['thickness3']." where id=$id";
+if ($_POST['thickness3'] != "") {
+    $sql = "UPDATE sheet SET thickness3=".$_POST['thickness3']." WHERE id=$id";
     $db -> query($sql);
 }
-if($_POST['thickness4'] != ""){
-    $sql = "update sheet set thickness4=".$_POST['thickness4']." where id=$id";
+if ($_POST['thickness4'] != "") {
+    $sql = "UPDATE sheet SET thickness4=".$_POST['thickness4']." WHERE id=$id";
     $db -> query($sql);
 }
 ### this concatenates existing notes, if any, with a new line including the date and the entered note text
-if($_POST['notes'] != ""){
-    $sql = "update notes set notetext= CONCAT(IFNULL(notetext,''),DATE_FORMAT(NOW(),'%m-%d-%y %T'),\" ".$_POST['notes']."\",'\n') where part_id=$id AND part_type=\"sheet\"";
+if ($_POST['notes'] != "") {
+    $sql = "UPDATE notes SET notetext= CONCAT(IFNULL(notetext,''),DATE_FORMAT(NOW(),'%m-%d-%y %T'),\" ".$_POST['notes']."\",'\n') WHERE part_id=$id AND part_type=\"sheet\"";
     $db -> query($sql);
 }
 
@@ -170,12 +170,12 @@ if ($_POST['lastEdit'] != "") {
 }
 
 ### if the name of the picture is not blank (i.e. a picture has been slotted to upload), perform several checks and upload
-if($_FILES['pic']['name'] != ""){
+if ($_FILES['pic']['name'] != "") {
     add_pic("sheet",$id,$_FILES,$_POST['picnotes']);
 }
 
 ### if the name of the file is not blank (i.e. a file has been slotted to upload), attempt to upload
-if(count($_FILES['files']['name'])){
+if (count($_FILES['files']['name'])){
     /*echo "file found?<br>";
     print_r($_FILES['files']);
     echo "<br><br>";
