0ef1c77fa0ab2a8a8045880c211b3fb8fc3b0bc6
Chase Nelson <chase.a.nelson@gmail.com>
2017-06-03 19:47:58 -0400

    functions.php re-required, actually needed

--- sheet.php
+++ sheet.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <?php
   require_once("database.php");
+  require_once("functions.php");
   $id=$_GET['id'];
   $db= new Database();
   $data=$db->db_query("SELECT * FROM sheet where id=$id");
