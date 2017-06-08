91ac30fe1284a3e6420a8bde5ab859e8cdd2f26d
Chase Nelson <chase.a.nelson@gmail.com>
2017-06-08 11:26:24 -0400

    Takes sheet JSON and converts it into an array

---
+++ js/getSheetInfo.js
@@ -0,0 +1,47 @@
+var dbJSON;
+var dbArray;
+  $.ajax({
+    url: '../php/getSheetData.php?id=6',
+    success: test,
+  })
+function test(response) {
+  dbJSON = JSON.parse(response);
+  console.log(dbJSON);
+
+  dbArray = [
+    dbJSON.name,
+    dbJSON.location,
+    dbJSON.dateCut,
+    dbJSON.ply,
+    dbJSON.mass_nb,
+    dbJSON.user_cut,
+    dbJSON.dateOven,
+    dbJSON.user_bagged,
+    dbJSON.num_wax_coats,
+    dbJSON.bagUseTimes,
+    dbJSON.curing_stackup,
+    dbJSON.ovenStart,
+    dbJSON.ovenReach107,
+    dbJSON.user_check1,
+    dbJSON.timeRamp,
+    dbJSON.user_ramp,
+    dbJSON.ovenReach177,
+    dbJSON.user_check2,
+    dbJSON.timeOvenOff,
+    dbJSON.user_check3,
+    dbJSON.timeRemoved,
+    dbJSON.user_remove,
+    dbJSON.lengthOutside,
+    dbJSON.lengthInside,
+    dbJSON.heightOutside,
+    dbJSON.heightInside,
+    dbJSON.mass_after,
+    dbJSON.thickness1,
+    dbJSON.thickness2,
+    dbJSON.thickness3,
+    dbJSON.thickness4,
+    dbJSON.bow,
+    dbJSON.user_measure,
+    dbJSON.lastEdit,
+  ];
+}
