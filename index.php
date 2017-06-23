588e6e304b2bd1b934ae0a80d075370d4284f6be
Chase Nelson <chase.a.nelson@gmail.com>
2017-06-23 14:41:31 -0400

    reformatted

--- index.php
+++ index.php
@@ -6,46 +6,46 @@
     <title>Purdue CMS FPix Phase II R&D Database</title>
   </head>
   <body>
-  <div id="wrapper">
-    <header>
-      <a href="index.php">
-        <img src="../phase_2/pics/pu_logo.jpg" width="200" height="100" alt="Purdue University Logo">
-        <img src="../phase_2/pics/CMS_logo_col.gif" width="100" height="100" alt="CMS Logo">
-      </a>
-  </header>
-    <nav>
-      <a href="part_list.php">Part List</a>
-      <br>
-      <a href="test_list.php">Test List</a>
-      <br>
-      <a href="https://docs.google.com/document/d/1zDu6hiUR7r6qumQPcKdV3OXh7vLpGjodTjLopjbufKQ/edit?usp=sharing" target="_blank">Project Logbook</a>
-      <br>
-      <a href="https://drive.google.com/drive/folders/0B04OIAGnMDYxbXBkTWJmMm5hN0E?usp=sharing" target="_blank">Project Google Drive</a>
-      <br>
-      <a href="contact.php">Contact/Issues</a>
-    </nav>
-    <main>
-      <h1>Purdue CMS FPix Phase II R&D Database</h1>
-      <h2>Part Information</h2>
-      <input class="button" type=button onClick="location.href='part_list.php'" value='Part List'>
-      <h2>Part Submission</h2>
-      <input class="button" type=button onClick="location.href='newsheet.php'" value='Add Sheet'>
-      <h2>Testing Information</h2>
-      <input class="button" type=button onClick="location.href='test_list.php'" value='Test List'>
-      <input class="button" type=button onClick="location.href='newtest.php'" value='New Test'>
-      <!-- <h2>Reference</h2> To Do: naming conventions, etc. -->
-      <h2>Links</h2>
-      <input class="button" type="button" value="Project Logbook"
-      onclick="window.open('https://docs.google.com/document/d/1zDu6hiUR7r6qumQPcKdV3OXh7vLpGjodTjLopjbufKQ/edit?usp=sharing')">
-      <br><br>
-      <input class="button" type="button" value="Project Google Drive"
-      onclick="window.open('https://drive.google.com/drive/folders/0B04OIAGnMDYxbXBkTWJmMm5hN0E?usp=sharing')">
-    </main>
+    <div id="wrapper">
+      <header>
+        <a href="index.php">
+          <img src="../phase_2/pics/pu_logo.jpg" width="200" height="100" alt="Purdue University Logo">
+          <img src="../phase_2/pics/CMS_logo_col.gif" width="100" height="100" alt="CMS Logo">
+        </a>
+    </header>
+      <nav>
+        <a href="part_list.php">Part List</a>
+        <br>
+        <a href="test_list.php">Test List</a>
+        <br>
+        <a href="https://docs.google.com/document/d/1zDu6hiUR7r6qumQPcKdV3OXh7vLpGjodTjLopjbufKQ/edit?usp=sharing" target="_blank">Project Logbook</a>
+        <br>
+        <a href="https://drive.google.com/drive/folders/0B04OIAGnMDYxbXBkTWJmMm5hN0E?usp=sharing" target="_blank">Project Google Drive</a>
+        <br>
+        <a href="contact.php">Contact/Issues</a>
+      </nav>
+      <main>
+        <h1>Purdue CMS FPix Phase II R&D Database</h1>
+        <h2>Part Information</h2>
+        <input class="button" type=button onClick="location.href='part_list.php'" value='Part List'>
+        <h2>Part Submission</h2>
+        <input class="button" type=button onClick="location.href='newsheet.php'" value='Add Sheet'>
+        <h2>Testing Information</h2>
+        <input class="button" type=button onClick="location.href='test_list.php'" value='Test List'>
+        <input class="button" type=button onClick="location.href='newtest.php'" value='New Test'>
+        <!-- <h2>Reference</h2> To Do: naming conventions, etc. -->
+        <h2>Links</h2>
+        <input class="button" type="button" value="Project Logbook"
+        onclick="window.open('https://docs.google.com/document/d/1zDu6hiUR7r6qumQPcKdV3OXh7vLpGjodTjLopjbufKQ/edit?usp=sharing')">
+        <br><br>
+        <input class="button" type="button" value="Project Google Drive"
+        onclick="window.open('https://drive.google.com/drive/folders/0B04OIAGnMDYxbXBkTWJmMm5hN0E?usp=sharing')">
+      </main>
       <footer>
         <p>This site uses HTML5. Please use Google Chrome, Mozilla Firefox, Safari, or IE9 (or greater).</p>
       </footer>
-  </div>
- </body>
+    </div>
+  </body>
 </html>
 
 
