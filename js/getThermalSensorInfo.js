32a13eb9e89b99f7a549a519bd5d01f3fd1b7201
Chase Nelson <chase.a.nelson@gmail.com>
2017-06-29 13:30:50 -0400

    changed what gets added to dbArray

--- js/getThermalSensorInfo.js
+++ js/getThermalSensorInfo.js
@@ -6,16 +6,12 @@ function JSONtoArray(response) {
   dbJSON = JSON.parse(response);
 
   keyArray = [
-    "Name",
-    "Sensor Type",
     "Color",
     "Current Channel",
     "Status"
   ];
 
   dbArray = [
-    dbJSON.name,
-    dbJSON.sensor_type,
     dbJSON.color,
     dbJSON.cur_channel,
     dbJSON.status
