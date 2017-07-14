f6ec50cea33cefee416acda1960b8c02264f084d
Chase Nelson <chase.a.nelson@gmail.com>
2017-07-14 13:49:40 -0400

    used a minified link for react

--- module.php
+++ module.php
@@ -62,7 +62,7 @@
     <script src="./node_modules/jquery/dist/jquery.min.js" charset="utf-8"></script>
     <script src="./js/getModuleInfo.min.js" charset="utf-8"></script>
     <script src="http://cdnjs.cloudflare.com/ajax/libs/es6-shim/0.11.0/es6-shim.min.js"></script>
-    <script src="http://fb.me/react-with-addons-0.11.0.js"></script>
+    <script src="http://fb.me/react-with-addons-0.11.0.min.js"></script>
     <script src="http://fb.me/JSXTransformer-0.11.0.js"></script>
     <script type="text/jsx;harmony=true">/** @jsx React.DOM */
     var id = <?php echo $id; ?>;
