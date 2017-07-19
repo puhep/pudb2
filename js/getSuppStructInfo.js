a3316eeadfd070f358a57f0c2c4460e41e5646f9
Chase Nelson <chase.a.nelson@gmail.com>
2017-07-19 13:58:45 -0400

    Added comments

--- js/getSuppStructInfo.js
+++ js/getSuppStructInfo.js
@@ -1,6 +1,6 @@
-var dbJSON;
-var dbArray;
-var keyArray;
+var dbJSON; // JSON object that holds all the values from the database
+var dbArray; // Array made of select values from dbJSON
+var keyArray; // Array to hold the key that matches the dbArray
 
 function JSONtoArray(response) {
   dbJSON = JSON.parse(response);
