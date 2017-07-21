a3f1b5a309474b181d35c005c07e571e79c20099
Chase Nelson <chase.a.nelson@gmail.com>
2017-07-21 13:59:41 -0400

    Added comments and white space

--- moduleSensorEditProc.php
+++ moduleSensorEditProc.php
@@ -1,4 +1,11 @@
 <?php
+  /******************
+  *
+  * This page checks to see what fields have been changed and then calls the database to update them.
+  *
+  * A faster way might be to add to a string and call then database once, instead of check, call, chech again...
+  *
+  ******************/
   $backmessage="Please press back and try again.<br>";
   require_once("database.php");
   require_once("functions.php");
@@ -9,14 +16,17 @@
     $sql="UPDATE mock_module SET si_thickness=".$_POST['curThickness']." WHERE id=$id";
     $db -> query($sql);
   }
+
   if ($_POST['curAdhesive'] != "") {
     $sql="UPDATE mock_module SET adhesive=\"".$_POST['curAdhesive']."\" WHERE id=$id";
     $db -> query($sql);
   }
+
   if ($_POST['curGeometry'] != "") {
     $sql="UPDATE mock_module SET geometry=\"".$_POST['curGeometry']."\" WHERE id=$id";
     $db -> query($sql);
   }
+
   if ($_POST['notes'] != "") {
     $sql="UPDATE notes SET notetext= CONCAT(IFNULL(notetext,''),DATE_FORMAT(NOW(), '%m-%d-%y %T'), \" ".$_POST['notes']."\",'\n') where part_id=$id AND part_type=\"mock_module\"";
     $db -> query($sql);
@@ -27,14 +37,16 @@
     $db->query($sql);
   }
 
+  // If the name of the picture is not blank (i.e. a picture has been slotted to upload), perform several checks and upload
   if ($_FILES['pic']['name'] != "") {
     add_pic("mock_module", $id, $_FILES, $_POST['picnotes']);
   }
 
+  // If the name of the file is not blank (i.e. a file has been slotted to upload), attempt to upload
   if ($_FILES['files']['name'][0] != "") {
     add_file("mock_module", $id, $_FILES['files']);
   }
 
-  ### redirect to the mock module summary page with the new information
+  // Redirect to the summary page with the new information
   header("Location: module.php?id=$id");
 ?>
--- sheet_edit_proc.php
+++ sheet_edit_proc.php
@@ -1,93 +1,97 @@
 <?php
-#ini_set('display_errors', 'On');
-#error_reporting(E_ALL | E_STRICT);
-$backmessage = "Please press back and try again.<br>";
-require_once("database.php");
-require_once("functions.php");
+  /******************
+  *
+  * This page checks to see what fields have been changed and then calls the database to update them.
+  *
+  * A faster way might be to add to a string and call then database once, instead of check, call, chech again...
+  *
+  ******************/
+  $backmessage = "Please press back and try again.<br>";
+  require_once("database.php");
+  require_once("functions.php");
 
-$id = $_POST['id'];
-$db = new Database();
+  $id = $_POST['id'];
+  $db = new Database();
 
-//print_r($_POST);
-### UPDATE miscellaneous info if the fields have been filled
-if ($_POST['name'] != "") {
+  // UPDATE miscellaneous info if the fields have been filled
+  if ($_POST['name'] != "") {
     $sql = "UPDATE sheet SET name=\"".$_POST['name']."\" WHERE id=$id";
     $db -> query($sql);
-}
-if ($_POST['location'] != "") {
+  }
+  if ($_POST['location'] != "") {
     $sql = "UPDATE sheet SET location = \"".$_POST['location']."\" WHERE id=$id";
     $db -> query($sql);
-}
+  }
 
-if ($_POST['dateCut'] != "") {
-  $sql = "UPDATE sheet SET dateCut=\"".$_POST['dateCut']."\" WHERE id=$id";
-  $db->query($sql);
-}
+  if ($_POST['dateCut'] != "") {
+    $sql = "UPDATE sheet SET dateCut=\"".$_POST['dateCut']."\" WHERE id=$id";
+    $db->query($sql);
+  }
 
-if ($_POST['dateOven'] != "") {
-  $sql = "UPDATE sheet SET dateOven=\"".$_POST['dateOven']."\" WHERE id=$id";
-  $db->query($sql);
-}
+  if ($_POST['dateOven'] != "") {
+    $sql = "UPDATE sheet SET dateOven=\"".$_POST['dateOven']."\" WHERE id=$id";
+    $db->query($sql);
+  }
 
-if ($_POST['bagUseTimes'] != "") {
-  $sql = "UPDATE sheet SET bagUseTimes=".$_POST['bagUseTimes']." WHERE id=$id";
-  $db->query($sql);
-}
+  if ($_POST['bagUseTimes'] != "") {
+    $sql = "UPDATE sheet SET bagUseTimes=".$_POST['bagUseTimes']." WHERE id=$id";
+    $db->query($sql);
+  }
 
-if ($_POST['ovenStart'] != "") {
-  $sql = "UPDATE sheet SET ovenStart=\"".$_POST['ovenStart']."\" WHERE id=$id";
-  $db->query($sql);
-}
+  if ($_POST['ovenStart'] != "") {
+    $sql = "UPDATE sheet SET ovenStart=\"".$_POST['ovenStart']."\" WHERE id=$id";
+    $db->query($sql);
+  }
 
-if ($_POST['ovenReach177'] != "") {
-  $sql = "UPDATE sheet SET ovenReach177=\"".$_POST['ovenReach177']."\" WHERE id=$id";
-  $db->query($sql);
-}
+  if ($_POST['ovenReach177'] != "") {
+    $sql = "UPDATE sheet SET ovenReach177=\"".$_POST['ovenReach177']."\" WHERE id=$id";
+    $db->query($sql);
+  }
 
-if ($_POST['ovenReach107'] != "") {
-  $sql = "UPDATE sheet SET ovenReach107=\"".$_POST['ovenReach107']."\" WHERE id=$id";
-  $db->query($sql);
-}
+  if ($_POST['ovenReach107'] != "") {
+    $sql = "UPDATE sheet SET ovenReach107=\"".$_POST['ovenReach107']."\" WHERE id=$id";
+    $db->query($sql);
+  }
 
-if ($_POST['timeRamp'] != "") {
-  $sql = "UPDATE sheet SET timeRamp=\"".$_POST['timeRamp']."\" WHERE id=$id";
-  $db->query($sql);
-}
+  if ($_POST['timeRamp'] != "") {
+    $sql = "UPDATE sheet SET timeRamp=\"".$_POST['timeRamp']."\" WHERE id=$id";
+    $db->query($sql);
+  }
 
-if ($_POST['timeOvenOff'] != "") {
-  $sql = "UPDATE sheet SET timeOvenOff=\"".$_POST['timeOvenOff']."\" WHERE id=$id";
-  $db->query($sql);
-}
+  if ($_POST['timeOvenOff'] != "") {
+    $sql = "UPDATE sheet SET timeOvenOff=\"".$_POST['timeOvenOff']."\" WHERE id=$id";
+    $db->query($sql);
+  }
 
-if ($_POST['timeRemoved'] != "") {
-  $sql = "UPDATE sheet SET timeRemoved=\"".$_POST['timeRemoved']."\" WHERE id=$id";
-  $db->query($sql);
-}
+  if ($_POST['timeRemoved'] != "") {
+    $sql = "UPDATE sheet SET timeRemoved=\"".$_POST['timeRemoved']."\" WHERE id=$id";
+    $db->query($sql);
+  }
 
-if ($_POST['lengthOutside'] != "") {
-  $sql = "UPDATE sheet SET lengthOutside=".$_POST['lengthOutside']." WHERE id=$id";
-  $db->query($sql);
-}
+  if ($_POST['lengthOutside'] != "") {
+    $sql = "UPDATE sheet SET lengthOutside=".$_POST['lengthOutside']." WHERE id=$id";
+    $db->query($sql);
+  }
 
-if ($_POST['lengthInside'] != "") {
-  $sql = "UPDATE sheet SET lengthInside=".$_POST['lengthInside']." WHERE id=$id";
-  $db->query($sql);
-}
+  if ($_POST['lengthInside'] != "") {
+    $sql = "UPDATE sheet SET lengthInside=".$_POST['lengthInside']." WHERE id=$id";
+    $db->query($sql);
+  }
 
-if ($_POST['heightOutside'] != "") {
-  $sql = "UPDATE sheet SET heightOutside=".$_POST['heightOutside']." WHERE id=$id";
-  $db->query($sql);
-}
+  if ($_POST['heightOutside'] != "") {
+    $sql = "UPDATE sheet SET heightOutside=".$_POST['heightOutside']." WHERE id=$id";
+    $db->query($sql);
+  }
 
-if ($_POST['heightInside'] != "") {
-  $sql = "UPDATE sheet SET heightInside=".$_POST['heightInside']." WHERE id=$id";
-  $db->query($sql);
-}
+  if ($_POST['heightInside'] != "") {
+    $sql = "UPDATE sheet SET heightInside=".$_POST['heightInside']." WHERE id=$id";
+    $db->query($sql);
+  }
 
-if ($_POST['bow'] != "") {
-  $sql = "UPDATE sheet SET bow=".$_POST['bow']." WHERE id=$id";
-  $db->query($sql);
-}
+  if ($_POST['bow'] != "") {
+    $sql = "UPDATE sheet SET bow=".$_POST['bow']." WHERE id=$id";
+    $db->query($sql);
+  }
 
   if ($_POST['checkedLeaks'] != "N/A") {
     $sql = "UPDATE sheet SET checkedLeaks=\"".$_POST['checkedLeaks']."\" WHERE id=$id";
@@ -109,104 +113,117 @@ if ($_POST['bow'] != "") {
     $db->query($sql);
   }
 
-if ($_POST['ply'] != "") {
+  if ($_POST['ply'] != "") {
     $sql = "UPDATE sheet SET ply=".$_POST['ply']." WHERE id=$id";
     $db -> query($sql);
-}
-if ($_POST['mass_nb'] != "") {
+  }
+
+  if ($_POST['mass_nb'] != "") {
     $sql = "UPDATE sheet SET mass_nb=".$_POST['mass_nb']." WHERE id=$id";
     $db -> query($sql);
-}
-if ($_POST['mass_after'] != "") {
+  }
+
+  if ($_POST['mass_after'] != "") {
     $sql = "UPDATE sheet SET mass_after=".$_POST['mass_after']." WHERE id=$id";
     $db -> query($sql);
-}
-if ($_POST['num_wax_coats'] != "") {
+  }
+
+  if ($_POST['num_wax_coats'] != "") {
     $sql = "UPDATE sheet SET num_wax_coats=".$_POST['num_wax_coats']." WHERE id=$id";
     $db -> query($sql);
-}
-if ($_POST['curing_stackup'] != "") {
+  }
+
+  if ($_POST['curing_stackup'] != "") {
     $sql = "UPDATE sheet SET curing_stackup=\"".$_POST['curing_stackup']."\" WHERE id=$id";
     $db -> query($sql);
-}
-if ($_POST['user_cut'] != "") {
+  }
+
+  if ($_POST['user_cut'] != "") {
     $sql = "UPDATE sheet SET user_cut=\"".$_POST['user_cut']."\" WHERE id=$id";
     $db -> query($sql);
-}
-if ($_POST['user_bagged'] != "") {
+  }
+
+  if ($_POST['user_bagged'] != "") {
     $sql = "UPDATE sheet SET user_bagged=\"".$_POST['user_bagged']."\" WHERE id=$id";
     $db -> query($sql);
-}
-if ($_POST['user_check1'] != "") {
-    #echo "found";
+  }
+
+  if ($_POST['user_check1'] != "") {
     $sql = "UPDATE sheet SET user_check1=\"".$_POST['user_check1']."\" WHERE id=$id";
     $db -> query($sql);
-}
-if ($_POST['user_check2'] != "") {
+  }
+
+  if ($_POST['user_check2'] != "") {
     $sql = "UPDATE sheet SET user_check2=\"".$_POST['user_check2']."\" WHERE id=$id";
     $db -> query($sql);
-}
-if ($_POST['user_check3'] != "") {
+  }
+
+  if ($_POST['user_check3'] != "") {
     $sql = "UPDATE sheet SET user_check3=\"".$_POST['user_check3']."\" WHERE id=$id";
     $db -> query($sql);
-}
-if ($_POST['user_ramp'] != "") {
+  }
+
+  if ($_POST['user_ramp'] != "") {
     $sql = "UPDATE sheet SET user_ramp=\"".$_POST['user_ramp']."\" WHERE id=$id";
     $db -> query($sql);
-}
-if ($_POST['user_measure'] != "") {
+  }
+
+  if ($_POST['user_measure'] != "") {
     $sql = "UPDATE sheet SET user_measure=\"".$_POST['user_measure']."\" WHERE id=$id";
     $db -> query($sql);
-}
-if ($_POST['user_remove'] != "") {
+  }
+
+  if ($_POST['user_remove'] != "") {
     $sql = "UPDATE sheet SET user_remove=\"".$_POST['user_remove']."\" WHERE id=$id";
     $db -> query($sql);
-}
-if ($_POST['thickness1'] != "") {
+  }
+
+  if ($_POST['thickness1'] != "") {
     $sql = "UPDATE sheet SET thickness1=".$_POST['thickness1']." WHERE id=$id";
     $db -> query($sql);
-}
-if ($_POST['thickness2'] != "") {
+  }
+
+  if ($_POST['thickness2'] != "") {
     $sql = "UPDATE sheet SET thickness2=".$_POST['thickness2']." WHERE id=$id";
     $db -> query($sql);
-}
-if ($_POST['thickness3'] != "") {
+  }
+
+  if ($_POST['thickness3'] != "") {
     $sql = "UPDATE sheet SET thickness3=".$_POST['thickness3']." WHERE id=$id";
     $db -> query($sql);
-}
-if ($_POST['thickness4'] != "") {
+  }
+
+  if ($_POST['thickness4'] != "") {
     $sql = "UPDATE sheet SET thickness4=".$_POST['thickness4']." WHERE id=$id";
     $db -> query($sql);
-}
-### this concatenates existing notes, if any, with a new line including the date and the entered note text
-if ($_POST['notes'] != "") {
+  }
+
+  // This concatenates existing notes, if any, with a new line including the date and the entered note text
+  if ($_POST['notes'] != "") {
     $sql = "UPDATE notes SET notetext= CONCAT(IFNULL(notetext,''),DATE_FORMAT(NOW(),'%m-%d-%y %T'),\" ".$_POST['notes']."\",'\n') WHERE part_id=$id AND part_type=\"sheet\"";
     $db -> query($sql);
-}
+  }
 
-if ($_POST['lastEdit'] != "") {
-  $sql = "UPDATE sheet SET lastEdit=\"".$_POST['lastEdit']."\" WHERE id=$id";
-  $db->query($sql);
-}
+  if ($_POST['lastEdit'] != "") {
+    $sql = "UPDATE sheet SET lastEdit=\"".$_POST['lastEdit']."\" WHERE id=$id";
+    $db->query($sql);
+  }
 
-### if the name of the picture is not blank (i.e. a picture has been slotted to upload), perform several checks and upload
-if ($_FILES['pic']['name'] != "") {
+  // If the name of the picture is not blank (i.e. a picture has been slotted to upload), perform several checks and upload
+  if ($_FILES['pic']['name'] != "") {
     add_pic("sheet",$id,$_FILES,$_POST['picnotes']);
-}
+  }
 
-### if the name of the file is not blank (i.e. a file has been slotted to upload), attempt to upload
-if ($_FILES['files']['name'][0] != "") {
-    // echo "file found?<br>";
-    // print_r($_FILES['files']['name']);
-    // echo "<br><br>";
-    // echo count($_FILES['files']['name']);
+  // If the name of the file is not blank (i.e. a file has been slotted to upload), attempt to upload
+  if ($_FILES['files']['name'][0] != "") {
     add_file("sheet",$id,$_FILES['files']);
-}
+  }
 
-if ($_FILES['thicknessContour']['name'] != "") {
-  addSheetThicknessContour($id, $_FILES['thicknessContour']);
-}
+  // If the thicknessContour file has been select, then attempt to upload it
+  if ($_FILES['thicknessContour']['name'] != "") {
+    addSheetThicknessContour($id, $_FILES['thicknessContour']);
+  }
 
-### redirect to the summary page with the new information
-header("Location: sheet.php?id=$id");
+  // Redirect to the summary page with the new information
+  header("Location: sheet.php?id=$id");
 ?>
