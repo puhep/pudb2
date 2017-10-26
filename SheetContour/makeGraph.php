7ce3bcf72d6efa4e21c92abe79192127b7d1eb50
Chase Nelson <chase.a.nelson@gmail.com>
2017-10-26 16:01:05 -0400

    makes output directory if it does not exists

--- SheetContour/makeGraph.php
+++ SheetContour/makeGraph.php
@@ -1,8 +1,13 @@
 <?php
   $id = $_GET['id'];
   $dir = "../../phase_2/files/sheet/$id/ThicknessContour.csv";
+  $out = "../../phase_2/pics/sheet/$id";
   $test = "none";
   $res = "";
+  if (!file_exists($out)) { 
+    mkdir($out);
+    chmod($out, 0777);
+  }
   if (file_exists($dir)) {
     $test = system("./ContourPlot ".$id, $res);
   }
