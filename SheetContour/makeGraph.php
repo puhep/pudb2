edc36b37ef7dd430367bd6597b97278d561e427e
Chase Nelson <chase.a.nelson@gmail.com>
2017-11-09 08:39:14 -0500

    forgot a semi-colon

--- SheetContour/makeGraph.php
+++ SheetContour/makeGraph.php
@@ -1,12 +1,12 @@
 <?php
   $id = $_GET['id'];
   $dir1 = "../../phase_2/files/sheet/$id/ThicknessContour.csv";
-  $dir2 = "../../phase_2/files/sheet/$id/BowContour.csv";
+  $dir2 = "../../phase_2/files/shee/$id/BowContour.csv";
   $out = "../../phase_2/pics/sheet/$id";
   $test  = "none";
   $test1 = "none";
   $res   = "";
-  $res1  = "":
+  $res1  = "";
   if (!file_exists($out)) {
     mkdir($out);
     chmod($out, 0777);
@@ -16,7 +16,7 @@
     $test = system("./ContourPlot $id \"ThicknessContour.csv\" \"Thickness Contour\" \"contourPlot.png\"", $res);
   }
   if (file_exists($dir2)) {
-    $test1 = system("./ContourPlot ".$id." \"BowContour.csv\" \"Bow Contour\" \"bowPlot.png\"", $res1);
+    $test1 = system("./ContourPlot $id \"BowContour.csv\" \"Bow Contour\" \"bowPlot.png\"", $res1);
   }
   $return = new stdClass;
   $return->test  = $test;
